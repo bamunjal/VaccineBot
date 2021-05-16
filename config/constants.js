29c009de5cb6daaa76a43d497b338b6a852db673
Bandhan Munjal <bamunjal1995@gmail.com>
2021-05-16 18:28:56 +0530

    Added code for specific vaccine

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-    interval: "*/1 * * * *",
+    interval: "*/30 * * * * *",
     districtId: 395,
     ageLimit: 20,
     whatsapp_api:"https://api.callmebot.com/whatsapp.php",
